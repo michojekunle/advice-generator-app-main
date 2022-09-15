4bbf3839e56a68ee39053e7afc29c591cc5f9688
AMD <michojekunle1@gmail.com>
2022-09-15 17:20:53 +0100

    done

--- main.js
+++ main.js
@@ -1,7 +1,40 @@
-const diceButton = document.querySelector('#generateAdviceButton');
-const slideShow = document.querySelector('#slideshow')
+tailwind.config ={
+    theme: {
+      colors : {
+        lightCyan: 'hsl(193, 38%, 86%)',
+        neonGreen: 'hsl(150, 100%, 66%)',
+        grayishBlue: 'hsl(217, 19%, 38%)',
+        darkGrayishBlue: 'hsl(217, 19%, 24%)',
+        darkBlue: 'hsl(218, 23%, 16%)'
+      },
+      extend: {
+        fontFamily: {
+          manrope: ['Manrope', 'sans-serif']
+        },
+        boxShadow: {
+          '3xl': '0 0 20px 4px hsl(150, 100%, 66%) 0 0 20px 4px hsl(150, 100%, 66%)  0 0 20px 4px hsl(150, 100%, 66%) 0 0 20px 4px hsl(150, 100%, 66%) ',
+        }
+      }
+    }
+  }
 
-diceButton.addEventListener('click', () => {
+const diceButton = document.querySelector('#generateAdviceButton');
+const slideShow = document.querySelector('#slideshow');
+
+let showSlideshow = false;
+
+const audio = new Audio();
+
+const audioSrc = [
+    'audios/Cinematic-Indie.mp3',
+    'audios/tunetank_53 (1).mp3',
+    'audios/tunetank_53 (2).mp3',
+    'audios/tunetank_53 (3).mp3',
+    'audios/tunetank_53 (4).mp3',
+    'audios/tunetank_53 (5).mp3',
+];
+
+const generateAdvice = () => {
     const xhr = new XMLHttpRequest();
 
     xhr.addEventListener('loadend', () => {
@@ -11,12 +44,27 @@ diceButton.addEventListener('click', () => {
         const { slip: { id, advice } } = data;
         
         document.getElementById('adviceID').innerText = id;
-        document.getElementById('adviceText').innerText = advice;
+        document.getElementById('adviceText').innerText = `"${advice}"`;
 
 
     })
     
     xhr.open('GET', 'https://api.adviceslip.com/advice', true);
     xhr.send();
+}
 
-})
+diceButton.addEventListener('click',() => {showSlideshow = false; audio.pause(); generateAdvice();});
+
+slideShow.addEventListener('click', () => {
+    showSlideshow = !showSlideshow;
+    audio.src = audioSrc[Math.floor(Math.random() * audioSrc.length)-1];    
+    generateAdvice();
+    const slideshowInterval = setInterval( () => {
+        if(showSlideshow) {
+            generateAdvice();
+            audio.play();
+            audio.loop = true; 
+        }   
+    }, 8000);
+});
+
